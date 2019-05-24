a13e0d69841008cfa3dacf8ca227cf10bb5015e2
Woodland <Ryley.Woodland@gov.bc.ca>
2019-05-24 10:53:39 -0700

    Edited function to handle when no temp.txt file is found.

--- frontend-demo/routes/on_load.js
+++ frontend-demo/routes/on_load.js
@@ -4,9 +4,22 @@ var request = require('request');
 
 router.get('/', function(req, res, next) {
     var fs = require('fs');
-    fs.readFile('./temp_files/temp.txt', 'utf-8', function(err, data) {
-        var split_data = data.split("\n");
-        res.send(JSON.stringify(split_data));
+    fs.access('./temp_files/temp.txt', error => {
+        //if file exists
+        if(!error) {
+            fs.readFile('./temp_files/temp.txt', 'utf-8', function(err, data) {
+                var split_data = data.split("\n");
+                res.send(JSON.stringify(split_data));
+            });
+        } else {
+            console.log(error);
+            fs.writeFile('./temp_files/temp.txt', '', {flag : 'wx'}, function () {
+                fs.readFile('./temp_files/temp.txt', 'utf-8', function(err, data) {
+                    var split_data = data.split("\n");
+                    res.send(JSON.stringify(split_data));
+                });
+            });
+        }
     });
 });
 
